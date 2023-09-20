751ea727765b189fafd3901998e418116572e645
tlips <thomas.lips@ugent.be>
2023-09-20 10:08:35 +0200

    randomize the base color hue for polyhaven textures

--- synthetic-cloth-data/synthetic_cloth_data/synthetic_images/scene_builder/surface.py
+++ synthetic-cloth-data/synthetic_cloth_data/synthetic_images/scene_builder/surface.py
@@ -18,7 +18,7 @@ class PolyhavenMaterials(AssetConfig):
 @dataclasses.dataclass
 class SurfaceConfig:
     size_range: Tuple[float, float] = (1, 3)
-    polyhaven_material_probability: float = 0.5
+    polyhaven_material_probability: float = 0.95
     polyhaven_materials: PolyhavenMaterials = PolyhavenMaterials()
 
 
@@ -33,16 +33,47 @@ def add_cloth_surface_to_scene(config: SurfaceConfig) -> bpy.types.Object:
     if np.random.rand() < config.polyhaven_material_probability and len(config.polyhaven_materials.asset_list) > 0:
         material_dict = np.random.choice(config.polyhaven_materials.asset_list)
         material = ab.load_asset(**material_dict)
+        assert isinstance(material, bpy.types.Material)
 
-        # TODO: change material base color to a random color?
+        # add a color mix node before the principled BSDF color
+        # to randomize the base color hue
+
+        # use multiply to limit the change in brightness (which is always an issue with addition)
+        # colors should be close to (1,1,1) to avoid darkening the material too much (this is the issue with multiplying..)
+        # so set value to 1 and keep saturation low.
+        hue = np.random.uniform(0, 1)
+        saturation = np.random.uniform(0.0, 0.5)
+        value = 1.0
+        base_hsv = (hue, saturation, value)
+        base_rgb = hsv_to_rgb(base_hsv)
+
+        multiply_node = material.node_tree.nodes.new("ShaderNodeMixRGB")
+        multiply_node.blend_type = "MULTIPLY"
+        multiply_node.inputs["Fac"].default_value = 1.0
+        multiply_node.inputs["Color2"].default_value = (*base_rgb, 1.0)
+
+        # map original input of the BSDF base color to the multiply node
+        color_input_node = material.node_tree.nodes["Principled BSDF"].inputs["Base Color"].links[0].from_node
+        color_input_node_socket = (
+            material.node_tree.nodes["Principled BSDF"]
+            .inputs["Base Color"]
+            .links[0]
+            .from_socket.identifier  # use identifier, names are not unique!
+        )
+        material.node_tree.links.new(color_input_node.outputs[color_input_node_socket], multiply_node.inputs["Color1"])
+
+        # map the output of the multiply node to the BSDF base color
+        material.node_tree.links.new(
+            material.node_tree.nodes["Principled BSDF"].inputs["Base Color"], multiply_node.outputs["Color"]
+        )
 
         # disable actual mesh displacements as they change the geometry of the surface
         # and are not used in collision checking, which can cause the cloth to become 'invisible' in renders
         material.cycles.displacement_method = "BUMP"
         plane.data.materials.append(material)
     else:
-        hsv = sample_hsv_color()
-        rgb = hsv_to_rgb(hsv)
-        ab.add_material(plane, color=rgb)
+        base_hsv = sample_hsv_color()
+        base_rgb = hsv_to_rgb(base_hsv)
+        ab.add_material(plane, color=base_rgb)
 
     return plane
