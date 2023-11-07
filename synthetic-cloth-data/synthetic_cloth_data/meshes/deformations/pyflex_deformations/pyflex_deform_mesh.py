aec162a363c387a02952ab4574a4b6309e8a4bff
tlips <thomas.lips@ugent.be>
2023-11-07 16:09:23 +0100

    add clothfunnels-like deformation

--- synthetic-cloth-data/synthetic_cloth_data/meshes/deformations/pyflex_deformations/pyflex_deform_mesh.py
+++ synthetic-cloth-data/synthetic_cloth_data/meshes/deformations/pyflex_deformations/pyflex_deform_mesh.py
@@ -25,11 +25,16 @@ logger = loguru.logger
 
 @dataclasses.dataclass
 class DeformationConfig:
-    max_fold_distance: float = 0.6  # should allow to fold the cloth in half
+    pass
 
+
+@dataclasses.dataclass
+class ARTFDeformationConfig(DeformationConfig):
     max_bending_stiffness: float = 0.025  # higher becomes unrealistic
     max_stretch_stiffness: float = 2.0
     max_drag: float = 0.00001  # higher -> cloth will start to fall down very sloooow
+    max_fold_distance: float = 0.6  # should allow to fold the cloth in half
+
     max_orientation_angle: float = np.pi / 4  # higher will make the cloth more crumpled
 
     fold_probability: float = 0.6
@@ -37,6 +42,15 @@ class DeformationConfig:
     flip_probability: float = 0.4
     lift_probability: float = 0.0
     max_lift_height: float = 0.2
+
+
+@dataclasses.dataclass
+class ClothFunnelsDeformationConfig(DeformationConfig):
+    max_bending_stiffness: float = 0.025  # higher becomes unrealistic
+    max_stretch_stiffness: float = 2.0
+    max_drag: float = 0.00001  # higher -> cloth will start to fall down very sloooow
+    max_height: float = 0.5
+    max_distance: float = 0.5
 
 
 def deform_mesh(
@@ -91,65 +105,66 @@ def deform_mesh(
     inverse_masses = 1.0 / masses
     cloth_system.set_masses(inverse_masses)
 
-    # separate the rotations, otherwise the y-rotation will be applied before the Z-rotation
-    # which can increase the angle of the Z-rotation and thus make the cloth more crumpled
-    rotation_matrix = SE3Container.from_euler_angles_and_translation(
-        np.array(
-            [
-                np.random.uniform(0, deformation_config.max_orientation_angle),
-                0,
-                np.random.uniform(0, deformation_config.max_orientation_angle),
-            ]
-        )
-    ).rotation_matrix
+    if isinstance(deformation_config, ARTFDeformationConfig):
+        # separate the rotations, otherwise the y-rotation will be applied before the Z-rotation
+        # which can increase the angle of the Z-rotation and thus make the cloth more crumpled
+        rotation_matrix = SE3Container.from_euler_angles_and_translation(
+            np.array(
+                [
+                    np.random.uniform(0, deformation_config.max_orientation_angle),
+                    0,
+                    np.random.uniform(0, deformation_config.max_orientation_angle),
+                ]
+            )
+        ).rotation_matrix
 
-    y_rotation_matrix = SE3Container.from_euler_angles_and_translation(
-        np.array([0, np.random.uniform(0, 2 * np.pi), 0])
-    ).rotation_matrix
+        y_rotation_matrix = SE3Container.from_euler_angles_and_translation(
+            np.array([0, np.random.uniform(0, 2 * np.pi), 0])
+        ).rotation_matrix
 
-    cloth_system.set_positions(cloth_system.get_positions() @ rotation_matrix @ y_rotation_matrix)
-    cloth_system.center_object()
+        cloth_system.set_positions(cloth_system.get_positions() @ rotation_matrix @ y_rotation_matrix)
+        cloth_system.center_object()
 
-    # drop mesh
-    # tolerance empirically determined
-    wait_until_scene_is_stable(pyflex_stepper=cloth_system.pyflex_stepper, max_steps=500, tolerance=0.05)
+        # drop mesh
+        # tolerance empirically determined
+        wait_until_scene_is_stable(pyflex_stepper=cloth_system.pyflex_stepper, max_steps=500, tolerance=0.05)
 
-    # fold towards a random point around the grasp point
-    if np.random.uniform() < deformation_config.fold_probability:
-        logger.debug("folding")
+        # fold towards a random point around the grasp point
+        if np.random.uniform() < deformation_config.fold_probability:
+            logger.debug("folding")
 
-        if np.random.uniform() < deformation_config.grasp_keypoint_vertex_probability:
-            # load keypoints from json file
-            json_path = undeformed_mesh_path.replace(".obj", ".json")
-            keypoints = json.load(open(json_path))["keypoint_vertices"]
-            grasp_particle_idx = random.choice(list(keypoints.values()))
-        else:
-            grasp_particle_idx = np.random.randint(0, n_particles)
+            if np.random.uniform() < deformation_config.grasp_keypoint_vertex_probability:
+                # load keypoints from json file
+                json_path = undeformed_mesh_path.replace(".obj", ".json")
+                keypoints = json.load(open(json_path))["keypoint_vertices"]
+                grasp_particle_idx = random.choice(list(keypoints.values()))
+            else:
+                grasp_particle_idx = np.random.randint(0, n_particles)
 
-        grasper = ParticleGrasper(pyflex_stepper)
-        grasper.grasp_particle(grasp_particle_idx)
+            grasper = ParticleGrasper(pyflex_stepper)
+            grasper.grasp_particle(grasp_particle_idx)
 
-        fold_distance = np.random.uniform(0.1, deformation_config.max_fold_distance)
+            fold_distance = np.random.uniform(0.1, deformation_config.max_fold_distance)
 
-        cloth_center = cloth_system.get_center_of_mass()
-        vertex_position = cloth_system.get_positions()[grasp_particle_idx]
-        center_direction = np.arctan2(cloth_center[2] - vertex_position[2], cloth_center[0] - vertex_position[0])
+            cloth_center = cloth_system.get_center_of_mass()
+            vertex_position = cloth_system.get_positions()[grasp_particle_idx]
+            center_direction = np.arctan2(cloth_center[2] - vertex_position[2], cloth_center[0] - vertex_position[0])
 
-        # 70% of time wihtin pi/3 of the center direction. folds outside of the mesh are less interesting.
-        fold_direction = np.random.normal(center_direction, np.pi / 6)
+            # 70% of time wihtin pi/3 of the center direction. folds outside of the mesh are less interesting.
+            fold_direction = np.random.normal(center_direction, np.pi / 6)
 
-        fold_vector = np.array([np.cos(fold_direction), 0, np.sin(fold_direction)]) * fold_distance
-        logger.debug(f"fold vector: {fold_vector}")
+            fold_vector = np.array([np.cos(fold_direction), 0, np.sin(fold_direction)]) * fold_distance
+            logger.debug(f"fold vector: {fold_vector}")
 
-        # don't fold all the way, as that makes the sim 'force it back to flat' due to the inifite weight of the grasped particle
-        grasper.circular_fold_particle(fold_vector, np.pi * 0.9)
-        grasper.release_particle()
+            # don't fold all the way, as that makes the sim 'force it back to flat' due to the inifite weight of the grasped particle
+            grasper.circular_fold_particle(fold_vector, np.pi * 0.9)
+            grasper.release_particle()
 
-    if np.random.uniform() < deformation_config.lift_probability:
-        lift_particle_idx = np.random.randint(0, n_particles)
-        grasper.grasp_particle(lift_particle_idx)
-        grasper.lift_particle(np.random.uniform(0.05, deformation_config.max_lift_height))
-        grasper.release_particle()
+        if np.random.uniform() < deformation_config.lift_probability:
+            lift_particle_idx = np.random.randint(0, n_particles)
+            grasper.grasp_particle(lift_particle_idx)
+            grasper.lift_particle(np.random.uniform(0.05, deformation_config.max_lift_height))
+            grasper.release_particle()
 
         if np.random.uniform() < deformation_config.flip_probability:
             # lift, flip and drop again to have occluded folds
@@ -164,6 +179,29 @@ def deform_mesh(
             cloth_system.set_positions(cloth_system.get_positions() + np.array([0, 0.5, 0]))
             wait_until_scene_is_stable(pyflex_stepper=cloth_system.pyflex_stepper, tolerance=0.05)
 
+    elif isinstance(deformation_config, ClothFunnelsDeformationConfig):
+        # drop with random theta orientation
+        y_rotation_matrix = SE3Container.from_euler_angles_and_translation(
+            np.array([0, np.random.uniform(0, 2 * np.pi), 0])
+        ).rotation_matrix
+
+        cloth_system.set_positions(cloth_system.get_positions() @ y_rotation_matrix)
+        cloth_system.center_object()
+        wait_until_scene_is_stable(pyflex_stepper=cloth_system.pyflex_stepper, max_steps=500, tolerance=0.05)
+        # grasp random particle and move along vector
+        particle_idx = np.random.randint(0, n_particles)
+        distance = np.random.uniform(0, deformation_config.max_distance)
+        height = np.random.uniform(0, deformation_config.max_height)
+        angle = np.random.uniform(0, 2 * np.pi)
+        offset = np.array([np.cos(angle) * distance, height, np.sin(angle) * distance])
+
+        grasper = ParticleGrasper(pyflex_stepper)
+        grasper.grasp_particle(particle_idx)
+        grasper.move_particle(grasper.get_particle_position() + offset)
+        grasper.release_particle()
+
+    else:
+        raise NotImplementedError(f"deformation config {deformation_config} not implemented")
     wait_until_scene_is_stable(pyflex_stepper=cloth_system.pyflex_stepper, max_steps=200, tolerance=0.05)
     cloth_system.center_object()
 
@@ -205,15 +243,18 @@ def generate_deformed_mesh(
     # # create json file
     flat_mesh_data = json.load(open(mesh_path.replace(".obj", ".json")))
     # write data to json file
+
+    mesh_area = get_mesh_projected_xy_area(output_path)
     data = {
         "keypoint_vertices": flat_mesh_data["keypoint_vertices"],
-        "area": get_mesh_projected_xy_area(output_path),
+        "area": mesh_area,
         "flat_mesh": {
             "relative_path": mesh_path.replace(f"{DATA_DIR}/", ""),
             "obj_md5_hash": flat_mesh_data["obj_md5_hash"],
             "area": flat_mesh_data["area"],  # duplication, but makes it easier to use later on..
         },
     }
+
     with open(str(output_path).replace(".obj", ".json"), "w") as f:
         json.dump(data, f)
 
@@ -242,8 +283,7 @@ if __name__ == "__main__":
         json.dump(data, open(metadata_path, "w"))
         print(f"Metadata written to {metadata_path}")
 
-        deformation_config = hydra.utils.instantiate(cfg["deformation_config"])
-        print(deformation_config)
+        deformation_config = hydra.utils.instantiate(cfg["deformation"])
         for id in tqdm.trange(cfg.start_id, cfg.start_id + cfg.num_samples):
 
             # call python method in new process to avoid memory buildup
